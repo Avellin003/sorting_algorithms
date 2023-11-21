4f91554ced554bd284e84b4fdc605cb89ef0a969
Avellin003 <uavellibonaparte@gmail.com>
2023-11-21 15:51:04 +0200

    updates

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,7 +10,7 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
 	int temp;
-	int swapped;
+	size_t swapped;
 
 	if (array == NULL || size < 2)
 		return;
@@ -25,11 +25,11 @@ void bubble_sort(int *array, size_t size)
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
-				swapped = 1;
+				swapped++;
 				print_array(array, size);
 			}
 		}
-		if (!swapped)
+		if (swapped == 0)
 			break;
 	}
 }
